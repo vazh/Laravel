0766bc836c5a4252a56764bc0e3a90186b0fcbfe
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2017-04-04 14:41:47 +0700

    feat(route/web): add test route for test

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,18 @@
 |
 */
 
+
+Route::get('files/{fileId}', 'FileServerController@serve')->name('files');
 Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 Route::get('login', function () {
     return view('login');
 })->name('login');
-Route::get('files/{fileId}', 'FileServerController@serve')->name('files');
-Route::get('report/{id}', function (){
+Route::get('report/{id}', function () {
     return view('report');
 })->name('report');
-Route::get('logout', function (){
+Route::get('test', 'TestController@test')->name('test');
+Route::get('logout', function () {
     return 'GOODBYE';
 })->name('logout');
